7a381633322323c75a2ea69687564b68b33dd2e1
Gabriel Mota <100182257+oGabrielMota@users.noreply.github.com>
2025-04-07 13:53:07 -0300

    Nova função

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
+
+function copiarTexto() {
+  const texto = document.getElementById("textoParaCopiar").innerText;
+
+  navigator.clipboard.writeText(texto)
+    .then(() => {
+      alert("Texto copiado!");
+    })
+    .catch(err => {
+      console.error("Erro ao copiar: ", err);
+    });
+}
