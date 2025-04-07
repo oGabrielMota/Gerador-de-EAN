5d53766edc8a0148a9cd18935ee52057ed2c1cb5
Gabriel Mota <100182257+oGabrielMota@users.noreply.github.com>
2025-04-07 14:01:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,4 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
-
-function copiarTexto() {
-  const texto = document.getElementById("textoParaCopiar").innerText;
-
-  navigator.clipboard.writeText(texto)
-    .then(() => {
-      alert("Texto copiado!");
-    })
-    .catch(err => {
-      console.error("Erro ao copiar: ", err);
-    });
 }
